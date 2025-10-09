f3f4b103f55c3ea0d7abdfc039a6b2d000c860fe
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2025-10-09 00:02:48 -0300

    COMEÇO

---
+++ JS/telaInicio.js
@@ -0,0 +1,7 @@
+const animation = lottie.loadAnimation({
+      container: document.getElementById('lottie-bg'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'paws animation.json'  // <-- aqui vai o arquivo JSON de patinhas
+    });
