f17a84e3bdf4fde143885dae12cefab96831c658
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2025-10-21 16:51:12 -0300

    CAIXA DE TEXTO BOTAO VALORES QSE PRONTAS

---
+++ JS/dropdown.js
@@ -0,0 +1,26 @@
+let caixasTexto = document.querySelector('.open');
+const conteudoCaixa = document.querySelector('.open-content');
+let botoes = document.querySelectorAll('.btnCaixa');
+botoes.forEach((botao) => {
+    botao.addEventListener('click', () => {
+
+        let estaAtivo = caixasTexto.classList.toggle('active');
+        if (botao.textContent === 'Compromisso') {
+            conteudoCaixa.textContent = 'Nosso compromisso é garantir o bem-estar e a felicidade do seu pet, oferecendo serviços de alta qualidade e cuidado dedicado.';
+        } else if (botao.textContent === 'Empatia') {
+            conteudoCaixa.textContent = 'Entendemos as necessidades únicas de cada pet e tratamos todos com amor e respeito, criando um ambiente acolhedor para eles.';
+        } else if (botao.textContent === 'Educação') {
+            conteudoCaixa.textContent = 'Acreditamos na importância da educação contínua para nossos colaboradores, garantindo que estejam sempre atualizados com as melhores práticas de cuidado animal.';
+        } else if (botao.textContent === 'Transparencia') {
+            conteudoCaixa.textContent = 'Valorizamos a transparência em todas as nossas interações, mantendo você informado sobre os cuidados e serviços prestados ao seu pet.';
+        }
+
+        if (estaAtivo) {
+            caixasTexto.style.display = 'block'; // mostrar quando ativo
+        } else {
+            caixasTexto.style.display = 'none'; // esconder quando inativo
+        }
+
+
+    });
+});
